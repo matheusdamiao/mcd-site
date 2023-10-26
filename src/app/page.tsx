e4d38a19de26f062235de23ea4c804107d3a25a2
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-10-26 14:41:52 -0300

    refactor: clean home page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,128 +3,15 @@
 import Head from 'next/head';
 import * as React from 'react';
 
-import Button from '@/components/buttons/Button';
-import UnderlineLink from '@/components/links/UnderlineLink';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
-  type Restaurant = {
-    id: number;
-    attributes: {
-      Description: string;
-      Name: string;
-      createdAt: '2023-10-10T21:03:05.244Z';
-      publishedAt: '2023-10-10T21:03:29.354Z';
-      updatedAt: '2023-10-10T21:03:29.359Z';
-    };
-  };
-  const [restaurants, setRestaurants] = React.useState<Restaurant[]>();
-
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:1337/api/restaurants');
-    const data = await response.json();
-    return data;
-  };
-
-  React.useEffect(() => {
-    fetchData().then((v) => setRestaurants(v.data));
-  }, []);
-
-  React.useEffect(() => {
-    setRestaurants(restaurants);
-  }, [restaurants]);
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const handleFormChange = (event: any) => {
-    if (event.target.name === 'email') {
-      setEmail(event.target.value);
-    }
-    if (event.target.name === 'password') {
-      setPassword(event.target.value);
-    }
-  };
-
-  const login = async (e: any) => {
-    e.preventDefault();
-    const loginData = {
-      identifier: email,
-      password: password,
-    };
-
-    const res = await fetch(`http://localhost:1337/api/auth/local`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(loginData),
-    });
-
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to login');
-    }
-    const data = await res.json();
-
-    return data;
-  };
-
   return (
     <main>
       <Head>
-        <title>Hi</title>
+        <title>MCD Contabilidade Consultiva</title>
       </Head>
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo />
-          <div>
-            {restaurants ? (
-              restaurants.map((res) => (
-                <div key={res.id}>
-                  <h1>{res.attributes.Name}</h1>
-                  <h2>{res.attributes.Description}</h2>
-                </div>
-              ))
-            ) : (
-              <Button isLoading={true}> Carregando </Button>
-            )}
-          </div>
-          <form>
-            <input
-              value={email}
-              name='email'
-              onChange={(e) => handleFormChange(e)}
-              type='email'
-            />
-            <input
-              type='password'
-              value={password}
-              name='password'
-              onChange={(e) => handleFormChange(e)}
-            />
-            <button onClick={(e) => login(e)}>Login</button>
-          </form>
-
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://matheusdamiao.com.br'>
-              Matheus Damião
-            </UnderlineLink>
-          </footer>
+          ok
         </div>
       </section>
     </main>
